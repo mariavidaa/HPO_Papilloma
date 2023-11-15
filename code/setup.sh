716d224508d9f86bc9eb6285228b653e3b0dec85
mariajosehidalgo <mariajose.hidalgo@uma.es>
2023-11-15 12:26:48 +0100

    Ficheros de las dependencias creados

--- code/setup.sh
+++ code/setup.sh
@@ -5,8 +5,11 @@ mkdir -p ../software
 mkdir -p ../software/python
 mkdir -p ../software/R
 
+# Limpieza de cache
+python3 -m pip cache purge
+
 # Instalar dependencias de Python del archivo pythonreqs
-pip install -r ../results/pythonreqs.txt --target=../software/python
+python3 -m pip install -r ../results/pythonreqs.txt --target=../software/python 1> /dev/null
 
 # Instalar dependencias de R del archivo Rreqs
-Rscript -e 'install.packages(readLines("../results/Rreqs.txt"), lib="../software/R")'
+Rscript -e 'install.packages(readLines("../results/Rreqs.txt"), repos="http://cran.us.r-project.org", dependencies=TRUE, lib="../software/R")' 1> /dev/null
